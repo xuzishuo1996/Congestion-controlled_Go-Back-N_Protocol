0f8811a33523318c8d9f5dee57d3ece45ec20d98
Zishuo Xu <xuzishuo1996@gmail.com>
2021-03-07 12:58:06 +0800

    change max packet length: should measure in bytes, not chars

--- src/shared/Constant.java
+++ src/shared/Constant.java
@@ -1,14 +1,15 @@
 package shared;
 
 public class Constant {
-    public static int MAX_SEGMENT_LENGTH = 500;   // measured in char
+    public static int MAX_SEGMENT_LENGTH = 500;   // measured in bytes
     public static int MODULO = 32;
 
     public static int SIZE_OF_INT = 4;  // measured in bytes
     public static int SIZE_OF_CHAR = 2; // measured in bytes
     public static int PACKET_HEADER_SIZE = SIZE_OF_INT * 3;    // 3 ints; also = ACK_SIZE
     public static int ACK_SIZE = PACKET_HEADER_SIZE;
-    public static int MAX_PAYLOAD_SIZE = SIZE_OF_CHAR * MAX_SEGMENT_LENGTH; // measured in bytes;
+    public static int EOT_SIZE = PACKET_HEADER_SIZE;
+    public static int MAX_PAYLOAD_SIZE = MAX_SEGMENT_LENGTH / SIZE_OF_CHAR; // measured in bytes;
     public static int MAX_DATA_PACKET_SIZE = PACKET_HEADER_SIZE + MAX_PAYLOAD_SIZE; // measured in bytes;
 
 //    enum TYPE {
